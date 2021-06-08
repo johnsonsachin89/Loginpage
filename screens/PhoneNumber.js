7278246b81de0fbf907acca654d8bc77f067a314
johnsonsachin89 <johnsonsachin89@gmail.com>
2021-06-08 14:43:19 +0530

    Initial commit

---
+++ screens/PhoneNumber.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function PhoneNumber(props)
+  {
+  const [phoneNumber, setPhoneNumber] = useState(null);
+
+  
+   return (
+         <View style={styles.View}>
+              <Text style={styles.text}>Enter Phone Number with country code</Text>
+                 <TextInput
+                   autoFocus
+                   style={styles.input}
+                   value={phoneNumber}
+                   onChangeText={setPhoneNumber}
+
+                  />
+                 <Button title="Phone Number Sign In" onPress={() => props.onSubmit(phoneNumber)} />
+        </View>
+          );
+   }
+  const styles = StyleSheet.create({
+  screen: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+           },
+  View:{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        },    
+  input: {
+          borderWidth: 2,
+          borderColor: 'lightblue',
+          width: 300,
+          marginVertical: 30,
+          fontSize: 25,
+          padding: 10,
+          borderRadius: 8,
+         },
+  text: {
+    fontSize: 20,
+  },
+});
